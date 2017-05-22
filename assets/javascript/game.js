77bf660f54b6dd4053d6c33a23206d68cd82b957
Ryan Strickler <ryanstrickler@Ryans-MacBook-Pro-2.local>
2017-05-22 02:48:31 -0400

    added hp bars and display divs

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,6 +3,7 @@ var oppTeam;
 var oppName;
 var playerActive;
 var oppActive;
+var turn = 0;
 var music = document.createElement("audio");
 music.setAttribute("src", "assets/music/battleTheme.mp3");
 music.addEventListener('ended', function () {
@@ -769,21 +770,23 @@ $("document").ready(function () {
         initialize();
     })
 
-    function initialize() {
+    function initialize() { //sets main dsipay, creates display divs, calls selectPokemon() when complete
         $("#redButton").remove();
         $("#blueButton").remove();
+        $("#mainDisplay").append("<div id=playerPokemonDisplay>");
+        $("#mainDisplay").append("<div id=oppPokemonDisplay>");
         $("#mainDisplay").append("<div class=col-xs-2 id=oppTrainerSpriteBox>");
         if (oppName === "Red") {
             $('#oppTrainerSpriteBox').css({
                 "background-image": "url(assets/images/red.png)",
-                "background-size": "50% 100%",
+                "background-size": "45% 100%",
                 "background-repeat": "no-repeat"
             });
         }
         else {
             $('#oppTrainerSpriteBox').css({
                 "background-image": "url(assets/images/blue.png)",
-                "background-size": "50% 100%",
+                "background-size": "45% 100%",
                 "background-repeat": "no-repeat"
             });
         }
@@ -797,22 +800,256 @@ $("document").ready(function () {
         }, 3500);
     }
 
-    function selectPokemon() {
+    function selectPokemon() { //appends select box and respective buttons, changes active pokemon, removes select box when complete
         $("#mainDisplay").append("<div class=col-xs-3 id=selectBox>");
         $("#selectBox").append("<button class=pokemon-button id=firstPokemon>");
-        $("#firstPokemon").html(playerTeam[0].name);
+        if (playerTeam[0].stats[1] / playerTeam[0].stats[0] == 0) {
+            $("#firstPokemon").css({
+                "background-color": "red"
+            });
+            playerTeam[0].status = "Fainted";
+        }
+        $("#firstPokemon").html(playerTeam[0].name + "</br>HP: " + playerTeam[0].stats[1] + "/" + playerTeam[0].stats[0] + " " + playerTeam[0].status);
         $("#selectBox").append("<button class=pokemon-button id=secondPokemon>");
-        $("#secondPokemon").html(playerTeam[1].name);
+        if (playerTeam[1].stats[1] == 0) {
+            $("#secondPokemon").css({
+                "background-color": "red"
+            });
+            playerTeam[1].status = "Fainted";
+        }
+        $("#secondPokemon").html(playerTeam[1].name + "</br>HP: " + playerTeam[1].stats[1] + "/" + playerTeam[1].stats[0] + " " + playerTeam[1].status);
         $("#selectBox").append("<button class=pokemon-button id=thirdPokemon>");
-        $("#thirdPokemon").html(playerTeam[2].name);
+        if (playerTeam[2].stats[1] == 0) {
+            $("#thirdPokemon").css({
+                "background-color": "red"
+            });
+            playerTeam[2].status = "Fainted";
+        }
+        $("#thirdPokemon").html(playerTeam[2].name + "</br>HP: " + playerTeam[2].stats[1] + "/" + playerTeam[2].stats[0] + " " + playerTeam[2].status);
         $("#selectBox").append("<button class=pokemon-button id=fourthPokemon>");
-        $("#fourthPokemon").html(playerTeam[3].name);
+        if (playerTeam[3].stats[1] == 0) {
+            $("#fourthPokemon").css({
+                "background-color": "red"
+            });
+            playerTeam[3].status = "Fainted";
+        }
+        $("#fourthPokemon").html(playerTeam[3].name + "</br>HP: " + playerTeam[3].stats[1] + "/" + playerTeam[3].stats[0] + " " + playerTeam[3].status);
         $("#selectBox").append("<button class=pokemon-button id=fifthPokemon>");
-        $("#fifthPokemon").html(playerTeam[4].name);
+        if (playerTeam[4].stats[1] == 0) {
+            $("#fifthPokemon").css({
+                "background-color": "red"
+            });
+            playerTeam[4].status = "Fainted";
+        }
+        $("#fifthPokemon").html(playerTeam[4].name + "</br>HP: " + playerTeam[4].stats[1] + "/" + playerTeam[4].stats[0] + " " + playerTeam[4].status);
         $("#selectBox").append("<button class=pokemon-button id=sixthPokemon>");
-        $("#sixthPokemon").html(playerTeam[5].name);
+        if (playerTeam[5].stats[1] == 0) {
+            $("#sixthPokemon").css({
+                "background-color": "red"
+            });
+            playerTeam[5].status = "Fainted";
+        }
+        $("#sixthPokemon").html(playerTeam[5].name + "</br>HP: " + playerTeam[5].stats[1] + "/" + playerTeam[5].stats[0] + " " + playerTeam[5].status);
         $("#selectBox").append("<button class=pokemon-button id=cancel>");
         $("#cancel").html("Cancel");
+
+        //begin button handlers
+
+        $("#firstPokemon").on("click", function () {
+            if (playerTeam[0].stats[1] > 0) {
+                playerActive = playerTeam[0];
+                $("#selectBox").remove();
+                $("#log").append("</br>Go " + playerTeam[0].name + "!");
+                populate();
+            }
+            else {
+                $("#log").append("</br>" + playerTeam[0].name + " doesn't have any energy to battle.");
+            }
+        })
+
+        $("#secondPokemon").on("click", function () {
+            if (playerTeam[1].stats[1] > 0) {
+                playerActive = playerTeam[1];
+                $("#selectBox").remove();
+                $("#log").append("</br>Go " + playerTeam[1].name + "!");
+                populate();
+            }
+            else {
+                $("#log").append("</br>" + playerTeam[1].name + " doesn't have any energy to battle.");
+            }
+        })
+
+        $("#thirdPokemon").on("click", function () {
+            if (playerTeam[2].stats[1] > 0) {
+                playerActive = playerTeam[2];
+                $("#selectBox").remove();
+                $("#log").append("</br>Go " + playerTeam[2].name + "!");
+                populate();
+            }
+            else {
+                $("#log").append("</br>" + playerTeam[2].name + " doesn't have any energy to battle.");
+            }
+        })
+
+        $("#fourthPokemon").on("click", function () {
+            if (playerTeam[3].stats[1] > 0) {
+                playerActive = playerTeam[3];
+                $("#selectBox").remove();
+                $("#log").append("</br>Go " + playerTeam[3].name + "!");
+                populate();
+            }
+            else {
+                $("#log").append("</br>" + playerTeam[3].name + " doesn't have any energy to battle.");
+            }
+        })
+
+        $("#fifthPokemon").on("click", function () {
+            if (playerTeam[4].stats[1] > 0) {
+                playerActive = playerTeam[4];
+                $("#selectBox").remove();
+                $("#log").append("</br>Go " + playerTeam[4].name + "!");
+                populate();
+            }
+            else {
+                $("#log").append("</br>" + playerTeam[4].name + " doesn't have any energy to battle.");
+            }
+        })
+
+        $("#sixthPokemon").on("click", function () {
+            if (playerTeam[5].stats[1] > 0) {
+                playerActive = playerTeam[5];
+                $("#selectBox").remove();
+                $("#log").append("</br>Go " + playerTeam[5].name + "!");
+                populate();
+            }
+            else {
+                $("#log").append("</br>" + playerTeam[5].name + " doesn't have any energy to battle.");
+            }
+        })
+
+        $("#cancel").on("click", function () {
+            if (playerActive !== undefined) {
+                if (playerActive.stats[1] !== 0) {
+                    $("#selectBox").remove();
+                }
+            }
+            else {
+                $("#log").append("</br>You must select a Pokemon to send out.");
+            }
+        })
+
+    } //end of switch pokemon function
+
+    function populate() {
+        setTimeout(function () {
+            $("#playerPokemonDisplay").append("<img id='backImg' src=" + playerActive.imageBack + ">");
+            var image = new Image();
+            image.src = playerActive.imageBack
+            image.onload = function () {
+                var height = this.naturalHeight;
+                $('#backImg').css({
+                    "height": height * 2.75,
+                });
+            }
+            $("#playerName").html(playerActive.name);
+            $("#playerStatus").html(playerActive.status);
+            $("#playerHPbox").show();
+            $("#playerHPdisplay").html("HP: " + playerActive.stats[1] + "/" + playerActive.stats[0]);
+            $('#playerHPbar').css({
+                "width": playerActive.stats[1] / playerActive.stats[0] * 285,
+                "background-color": "green"
+            });
+            if (playerActive.stats[1] / playerActive.stats[0] <= .5) {
+                $('#playerHPbar').css({
+                    "background-color": "yellow"
+                });
+            }
+            if (playerActive.stats[1] / playerActive.stats[0] <= .1) {
+                $('#playerHPbar').css({
+                    "background-color": "red"
+                });
+            }
+            if (oppActive == undefined) {
+                initializeOpp();
+                setTimeout(function () {
+                    $("#menu").append("<button class=btn-default id=menuButton1>");
+                    $("#menuButton1").html(playerActive.moves[0].name);
+                    $("#menu").append("<button class=btn-default id=menuButton2>");
+                    $("#menuButton2").html(playerActive.moves[1].name);
+                    $("#menu").append("<button class=btn-default id=menuButton3>");
+                    $("#menuButton3").html(playerActive.moves[2].name);
+                    $("#menu").append("<button class=btn-default id=menuButton4>");
+                    $("#menuButton4").html(playerActive.moves[3].name);
+                    $("#menu").append("<button class=btn-default id=menuButtonSwitch>");
+                    $("#menuButtonSwitch").html("Switch Pokemon");
+                }, 2000);
+            }
+            else {
+                $("#menu").append("<button class=btn-default id=menuButton1>");
+                $("#menuButton1").html(playerActive.moves[0].name);
+                $("#menu").append("<button class=btn-default id=menuButton2>");
+                $("#menuButton2").html(playerActive.moves[1].name);
+                $("#menu").append("<button class=btn-default id=menuButton3>");
+                $("#menuButton3").html(playerActive.moves[2].name);
+                $("#menu").append("<button class=btn-default id=menuButton4>");
+                $("#menuButton4").html(playerActive.moves[3].name);
+                $("#menu").append("<button class=btn-default id=menuButtonSwitch>");
+                $("#menuButtonSwitch").html("Switch Pokemon");
+                $("#playerName").html(playerActive.name);
+                $("#playerStatus").html(playerActive.status);
+                $("#playerHPbox").show();
+                $("#playerHPdisplay").html("HP: " + playerActive.stats[1] + "/" + playerActive.stats[0]);
+                $('#playerHPbar').css({
+                    "width": playerActive.stats[1] / playerActive.stats[0] * 285,
+                    "background-color": "green"
+                });
+                if (playerActive.stats[1] / playerActive.stats[0] <= .5) {
+                    $('#playerHPbar').css({
+                        "background-color": "yellow"
+                    });
+                }
+                if (playerActive.stats[1] / playerActive.stats[0] <= .1) {
+                    $('#playerHPbar').css({
+                        "background-color": "red"
+                    });
+                }
+            }
+        }, 1500);
+    }
+
+    function initializeOpp() {
+        oppActive = oppTeam[Math.floor(Math.random() * 5.99)];
+        $("#log").append("</br>Pokemon Trainer " + oppName + " sent out " + oppActive.name + "!");
+        setTimeout(function () {
+            $("#oppPokemonDisplay").append("<img id='frontImg' src=" + oppActive.imageFront + ">");
+            var image = new Image();
+            image.src = oppActive.imageFront;
+            image.onload = function () {
+                var height = this.naturalHeight;
+                $('#frontImg').css({
+                    "height": height * 2,
+                });
+            }
+            $("#oppName").html(oppActive.name);
+            $("#oppStatus").html(oppActive.status);
+            $("#oppHPbox").show();
+            $("#oppHPdisplay").html("HP: " + oppActive.stats[1] + "/" + oppActive.stats[0]);
+            $('#oppHPbar').css({
+                "width": oppActive.stats[1] / oppActive.stats[0] * 285,
+                "background-color": "green"
+            });
+            if (oppActive.stats[1] / oppActive.stats[0] <= .5) {
+                $('#oppHPbar').css({
+                    "background-color": "yellow"
+                });
+            }
+            if (oppActive.stats[1] / oppActive.stats[0] <= .1) {
+                $('#oppHPbar').css({
+                    "background-color": "red"
+                });
+            }
+        }, 1500)
+
     }
 
 })
